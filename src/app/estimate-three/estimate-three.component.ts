7303d323f84b4e5786d7aae073c9b5d19d10b99e
charles <charlesrudisel@gmail.com>
2021-01-24 22:01:02 -0500

    For the sake

--- src/app/estimate-three/estimate-three.component.ts
+++ src/app/estimate-three/estimate-three.component.ts
@@ -10,6 +10,11 @@ import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 export class EstimateThreeComponent implements OnInit {
 
   array_of_selected_accounts: Array<string>;
+  insurance;
+  member_ID;
+  group_num;
+  x = [];
+  y = [];
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
@@ -24,5 +29,39 @@ export class EstimateThreeComponent implements OnInit {
 
     console.log(this.array_of_selected_accounts)
   }
+
+  getValue1(event: any){
+    this.insurance = event.target.value;
+    console.log(this.insurance)
+
+  }
+  getValue2(event: any){
+    this.member_ID = event.target.value
+    console.log(this.member_ID)
+
+  }
+
+  getValue3(event: any){
+    this.group_num = event.target.value
+    console.log(this.group_num)
+
+  }
+
+  toggleEditable(event: any) {
+
+    this.x = event.target.value;
+    
+   
+    console.log(this.x.toString())
+  }
+
+  toggleEditable2(event: any) {
+
+    this.y = event.target.value;
+    
+   
+    console.log(this.y.toString())
+  }
+
 
 }
